5cec3a7d306d8bd621340b076579cd8f3e68abfe
IagoFP <iagofranciscoperez123@gmail.com>
2025-07-13 18:12:30 +0200

    Variables en python

---
+++ 01_variables.py
@@ -0,0 +1,24 @@
+# diferentes tipos de variables en Python
+# variables de tipo cadena, entero y booleano
+my_string_variable = "Esto es una cadena de texto en Python"
+print(my_string_variable)
+
+my_int_variable = 10
+print(my_int_variable)
+
+my_bool_variable = True
+print(my_bool_variable)
+
+#concatenación de variables
+nombre = "Iago"
+edad = 12
+print("Hola, mi nombre es", nombre, "y tengo", edad, "anhos.")
+
+# algunas funciones del sistema
+print(len(my_string_variable))  # longitud de la cadena
+print(type(my_string_variable))  # tipo de la variable
+
+# inputs
+
+nombre = input('¿Cual es tu nombre?: ')
+print("Hola,", nombre)
