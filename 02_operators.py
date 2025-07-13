14391245a7e9b9ff4d5c0a1e574da7d0ddb81fca
IagoFP <iagofranciscoperez123@gmail.com>
2025-07-13 18:21:45 +0200

    operadores en python

---
+++ 02_operators.py
@@ -0,0 +1,18 @@
+# Operadores
+
+print(2 + 3)  # Suma
+print(5 - 2)  # Resta
+print(4 * 3)  # Multiplicación
+print(10 / 2)  # División
+print(10 // 3)  # División entera
+print(10 % 2)  # Modulo
+print(2 ** 3)  # Exponente
+
+# Operadores de comparación
+
+print(5 > 3)  # Mayor que
+print(5 < 3)  # Menor que
+print(5 >= 3)  # Mayor o igual que
+print(5 <= 3)  # Menor o igual que
+print(5 == 3)  # Igual que  
+print(5 != 3)  # Diferente que
