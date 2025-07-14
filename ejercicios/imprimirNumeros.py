bda2e069b80efdff6196ac2215e479c6859cfee4
IagoFP <iagofranciscoperez123@gmail.com>
2025-07-14 23:57:05 +0200

    Ejercicio de imprimir numeros con bucles

---
+++ ejercicios/imprimirNumeros.py
@@ -0,0 +1,12 @@
+# Ejercicio: Imprimir números del 1 al 100
+
+#FOR
+for numero in range(1, 101):
+    print(numero)
+
+
+# WHILE
+contador = 1
+while(contador <= 100):
+    print(contador)
+    contador += 1
