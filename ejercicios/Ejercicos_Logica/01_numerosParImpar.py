0a5f3d43c443cca3160a742110655d835298188a
IagoFP <iagofranciscoperez123@gmail.com>
2025-07-15 01:10:57 +0200

    Añadida la carpteta ejercicios de logica y ejercicos para saber si un numero es par o impar

---
+++ ejercicios/Ejercicos_Logica/01_numerosParImpar.py
@@ -0,0 +1,7 @@
+#Pide un número al usuario y muestra si es par o impar.
+numero = int(input("Introduce un número: "))
+
+if (numero % 2 == 0):
+    print(f"El número {numero} es par.")
+else:
+    print(f"El número {numero} es impar.")
